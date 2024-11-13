6e5b49a54828a6f8ad8685d84dc0a070a5bb805d
Jack Jaramillo <mayercodes@gmail.com>
2024-11-13 12:37:57 -0500

    Agregar componente MyFirst y mostrar el ciclo de vida de un componente

--- etapa-2/mi-primer-proyecto/src/app/app.module.ts
+++ etapa-2/mi-primer-proyecto/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { MyFirstComponent } from './components/my-first/my-first.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponent
   ],
   imports: [
     BrowserModule,
---
+++ etapa-2/mi-primer-proyecto/src/app/components/my-first/my-first.component.ts
@@ -0,0 +1,36 @@
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-my-first',
+  templateUrl: './my-first.component.html',
+  styleUrls: ['./my-first.component.scss']
+})
+export class MyFirstComponent implements OnInit, AfterViewInit, OnDestroy {
+  constructor () {
+    // inicializar variables
+    // injección de dependencias
+    console.log('constructor')
+  }
+
+  ngOnInit (): void {
+    // inicializar parametros
+    // iniciar variables
+    // llamar a servicios
+    // peticiones http
+    console.log('ngOnInit')
+  }
+
+  ngAfterViewInit (): void {
+    // manipular elementos del DOM
+    console.log('ngAfterViewInit')
+  }
+
+  ngOnDestroy (): void {
+    // limpiar memoria
+    // cancelar peticiones http
+    // cancelar suscripciones
+    // eliminar listeners
+    // eliminar intervalos
+    console.log('ngOnDestroy')
+  }
+}
