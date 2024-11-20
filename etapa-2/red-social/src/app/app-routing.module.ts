acdc960168e005f472347de6acdf312979a7bfd8
Jack Jaramillo <mayercodes@gmail.com>
2024-11-20 13:15:30 -0500

    Agregar componentes de posts, cuenta y mis publicaciones con sus respectivas plantillas y módulos; configurar rutas para la navegación

--- etapa-2/red-social/src/app/app-routing.module.ts
+++ etapa-2/red-social/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'myposts',
+    loadChildren: () => import('./my-posts/my-posts.module').then(m => m.MyPostsModule)
+  },
+  {
+    path: 'posts',
+    loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
