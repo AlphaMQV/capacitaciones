4b0c6914be4a2b4d151a002068bf187076f7c213
Jack Jaramillo <mayercodes@gmail.com>
2024-11-22 13:03:43 -0500

    Agregar componente Home con formularios reactivos y de plantilla; actualizar rutas y módulos correspondientes

--- etapa-2/red-social/src/app/app.module.ts
+++ etapa-2/red-social/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -18,7 +19,9 @@ import { MaterialModule } from './material/material.module'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- etapa-2/red-social/src/app/components/home/home.component.ts
+++ etapa-2/red-social/src/app/components/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core'
+import { FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms'
+
+interface FormI {
+  names: FormControl
+  age: FormControl
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +12,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private readonly _formBuilder = inject(NonNullableFormBuilder)
 
-  constructor() { }
+  formReactive: FormGroup<FormI> = this._formBuilder.group({
+    names: [''],
+    age: ['']
+  })
 
-  ngOnInit(): void {
+  ngOnInit (): void {
   }
 
+  handleSubmitReactive (): void {
+    console.log(this.formReactive.value)
+  }
+
+  handleSubmitTemplate (): void {
+    console.log('Form submitted template')
+  }
+
+  get formControls (): FormI {
+    return this.formReactive.controls
+  }
 }
