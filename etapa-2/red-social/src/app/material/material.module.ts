a99a8074144bec7ab7c86b5a7719cb54907c06af
Jack Jaramillo <mayercodes@gmail.com>
2024-11-28 11:46:11 -0500

    Agregar diseño y funcionalidad para la creación de nuevas publicaciones; actualizar estilos y estructura de componentes

--- etapa-2/red-social/src/app/material/material.module.ts
+++ etapa-2/red-social/src/app/material/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
@@ -11,7 +12,8 @@ import { MatToolbarModule } from '@angular/material/toolbar'
     MatIconModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
