369f17c70620a658cea21c78e4109f566b2cfdce
Jack Jaramillo <mayercodes@gmail.com>
2024-12-03 11:39:38 -0500

    Agregar MatDividerModule y mejorar el diseño del componente CardPost; incluir botones de interacción y ajustar estilos

--- etapa-2/red-social/src/app/material/material.module.ts
+++ etapa-2/red-social/src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
+import { MatDividerModule } from '@angular/material/divider'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
@@ -13,7 +14,8 @@ import { MatToolbarModule } from '@angular/material/toolbar'
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatDividerModule
   ]
 })
 export class MaterialModule { }
