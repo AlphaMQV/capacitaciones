1ec99956d9a78d6491380b475255c512b4ea69ba
Jack Jaramillo <mayercodes@gmail.com>
2024-12-04 11:58:38 -0500

    Agregar componente de autenticación; implementar lógica para alternar entre inicio de sesión y registro, y mejorar estilos del formulario

--- etapa-2/red-social/src/app/pages/account/auth/auth.component.ts
+++ etapa-2/red-social/src/app/pages/account/auth/auth.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent {
+  private _formMode: 'login' | 'register' = 'login'
 
-  constructor() { }
+  // --------------------------- methods ---------------------------
 
-  ngOnInit(): void {
+  onLoginMode (): void {
+    this._formMode = 'login'
   }
 
+  onRegisterMode (): void {
+    this._formMode = 'register'
+  }
+
+  // --------------------------- helpers ---------------------------
+
+  get isLoginMode (): boolean {
+    return this._formMode === 'login'
+  }
+
+  get isRegisterMode (): boolean {
+    return this._formMode === 'register'
+  }
 }
--- etapa-2/red-social/src/app/pages/account/auth/auth.module.ts
+++ etapa-2/red-social/src/app/pages/account/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { MaterialModule } from 'src/app/material/material.module'
 import { AuthComponent } from './auth.component'
 import { LoginComponent } from './login/login.component'
 import { RegisterComponent } from './register/register.component'
@@ -11,7 +12,8 @@ import { RegisterComponent } from './register/register.component'
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   exports: [
     AuthComponent
